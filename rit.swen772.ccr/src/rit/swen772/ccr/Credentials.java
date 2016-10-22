d8fda3931923ec5996a652b6e2232ac7fd23a393
Jairo Pavel Veloz Vidal <jpavelw@gmail.com>
2016-10-22 04:03:21 -0400

    Credential class added to .gitignore

--- rit.swen772.ccr/src/rit/swen772/ccr/Credentials.java
+++ rit.swen772.ccr/src/rit/swen772/ccr/Credentials.java
@@ -2,7 +2,7 @@ package rit.swen772.ccr;
 
 public class Credentials {
 	 
-	public static final String USER_NAME = "";
+	public static final String USER_NAME = "jpavelw";
 	public static final String PASSWORD = "";
 	public static final String TOKEN = "";
 	
